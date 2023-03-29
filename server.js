6834e7940ae7a40506f853c4bc43eaf524c09b2c
natalia vieira <verkappte@hotmail.com>
2023-03-29 14:35:41 -0300

    Inclusão bib URL e de comentários linha a linha

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
+//Blocos estruturais até linha 9
+
+//Incluindo uma biblioteca (do http)
 const http = require('http');
+const url = require('url'); //(from NPNJS)
 
+//Definição do IP e porta onde o código será executado (endereço/url)
 const hostname = '127.0.0.1';
-
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World LEIAA\n');
+//implementação da regra de negócios(assunto principal)
+const server = http.createServer((req, res) => { //Cria servidor em que executa o bloco abaixo ao acessar o localhost 3000
+
+    //Pegar a pergunta na url
+    console.log(req.url); //impressão no PowerShell 
+    //Verificar a pergunta e escolher uma resposta
+    //Retornar a resposta escolhida
+
+    res.statusCode = 200; //Código HTTP ("ok")
+    res.setHeader('Content-Type', 'text/plain'); //código
+    res.end('Hello, World LEIAA\n'); //impressão no navegador
 })
 
+//Bloco de sustentação (após toda a configuração acima, roda o servidor) -- EXECUÇÃO --
 server.listen(port, hostname, () => {
     console.log(`Server running http://${hostname}:${port}/`);
 });
